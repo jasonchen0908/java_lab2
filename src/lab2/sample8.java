528343dad1eeecd044137b70eb4281dd005fa532
葉宸豪 <ha@yechenhaode-MacBook-Pro.local>
2019-10-03 11:13:07 +0800

    java1

---
+++ src/lab2/sample8.java
@@ -0,0 +1,15 @@
+package lab2;
+
+public class sample8 
+{
+	public static void main(String[]args)
+	{
+	int num=3 ;
+	    
+	System.out.println("變數num的數值是"+num)	;
+	
+	num=5;
+	System.out.println("更新變數num的數值")	;
+	System.out.println("變數num更新後的數值是"+num)	;
+	}
+}
